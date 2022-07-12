71a71db13dc2daa327ee1e6bd19ac6de06058634
Dingdanginline <bioyanjingzhi@gmail.com>
2022-07-12 15:14:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,73 @@
 import discord
-from discord.ext import tasks
-import os
-from dotenv import load_dotenv
-import requests
+from discord.ext import commands
+import datetime
+
 import json
-import aiohttp
+from urllib.request import Request, urlopen
 
-load_dotenv()
+#-----------------------------------------------------------------------------
+#- Config:                                                                   -
+#-      name_prj     - The name of the project                               -
+#-      token_adr    - The address of the token                              -
+#-      abbreviation - The abbreviation of the token                         -
+#-      
+- Emoji for beautify purpose                            -
+#-----------------------------------------------------------------------------
 
-TOKEN = os.getenv('DISCORD_TOKEN')
-REFRESH_TIMER = os.getenv('REFRESH_TIMER')
-CONTRACT = os.getenv('CONTRACT')
-NAME = os.getenv('NAME')
-CHAIN = os.getenv('CHAIN')
-CURRENCY = os.getenv('CURRENCY')
+name_prj = "rvtbot"
+token_adr = "0x5d4eaf65b348be3889193913ef366d306a9fbd73"
+abbreviation = "RVT"
+emoji = ""
 
-client = discord.Client()
 
-async def get_price():
-    async with aiohttp.ClientSession() as session:
-        async with session.get(f"https://api.pancakeswap.info/api/v2/tokens/0x5d4eaf65b348be3889193913ef366d306a9fbd73") as r:
-            if r.status == 200:
-                js = await r.json()
-                price = js["data"]["price"]
-                pricestring = (f"{NAME}: ${price}")
-                return pricestring
+url = "https://api.pancakeswap.info/api/v2/tokens/"+token_adr
 
-@client.event
+
+bot = commands.Bot(command_prefix='!', description="This is the price bot for "+name_prj+", best project in the DeFi world!", help_command=None)
+
+
+#Price-message:
+@bot.command()
+async def price(ctx):
+    #j_token : json with token data-------------------------------------------------
+    req = Request(url_cow, headers={'User-Agent': 'Mozilla/5.0'})
+    webpage = urlopen(req).read()
+
+    j_token = json.loads(webpage)
+    #---------------------------------------------------------------------------
+
+    embed = discord.Embed()
+
+    embed.description ="""
+    **${:s}** {:s}
+    Price: $ {:f}
+    Price in BNB: {:f}
+    💱 [Buy ${:s}](https://pancakeswap.finance/swap?outputCurrency={:s})
+    📈 [Poocoin chart](https://poocoin.app/tokens/{:s})
+    """.format(abbreviation, emoji, round(float(j_token["data"]["price"]), 6), round(float(j_token["data"]["price_BNB"]), 6), abbreviation, token_adr, token_adr)
+
+    await ctx.send(embed=embed)
+
+@bot.command()
+async  def  help(ctx):
+    des = """
+    Commands for Crypto Token Price Bot:\n
+     **Prefix**:       !\n
+     **!price**:       Get information about tokens price\n
+    """
+    embed = discord.Embed(title="I'm the price bot for "+name_prj+", best project in the DeFi world!",url="##########",description= des,
+    timestamp=datetime.datetime.utcnow(),
+    color=discord.Color.blue())
+    embed.set_footer(text="Made by v0st0k1")
+
+
+    await ctx.send(embed=embed)
+
+@bot.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord! ')
-    for guild in client.guilds:
-        print("connected to ", guild.name)
-    refresh_price.start()
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="!help"))
+    print('The Crypto Token Price Bot is ready!')
 
-@tasks.loop(seconds=float(REFRESH_TIMER))
-async def refresh_price():
-    for guild in client.guilds:
-        await guild.me.edit(nick=await get_price())
-client.run(TOKEN)
+
+
+bot.run('OTgzMTgzNzY0MTgyMTM4OTMx.GBrvcD.tmKI2oxZPRDKN3TqF5UJeM7SOBxIU8M3f4pVpg')
